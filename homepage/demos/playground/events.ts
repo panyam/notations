73c0fd0c3529be0c4daac1f531c0e4e116d6c5dc
Sri Panyam <sri.panyam@gmail.com>
2026-01-05 11:53:30 -0800

    Removing legacy homepage

--- homepage/demos/playground/events.ts
+++
@@ -1,7 +0,0 @@
-
-export const ParserCompiled = "ParserCompiled";
-export const InputParsed = "InputParsed";
-export const InputSaved = "InputSaved";
-export const GrammarParsed = "GrammarParsed";
-export const GrammarSelected = "GrammarSelected";
-export const Log = "Log";
